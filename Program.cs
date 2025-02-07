dc2fc0067471fdda273f59db237553a3a77b2396
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-07 19:32:27 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Este programa utiliza a API "Game of Thrones" para obter informações sobre um personagem específico,
+// identificado pelo ID 583. O objetivo é realizar uma requisição HTTP para recuperar e exibir
+// os recursos do personagem.
+// A implementação faz uso da classe HttpClient para enviar a solicitação e processar a resposta,
+// garantindo que os dados sejam tratados corretamente.
+using(HttpClient client = new HttpClient())
+{
+    string resposta = await client.GetStringAsync("https://anapioficeandfire.com/api/characters/583");
+    Console.WriteLine(resposta);
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Consumo da API 'Game of Thrones' para Obtenção de Personagem
+
+Este programa utiliza a API "Game of Thrones" para obter informações sobre um personagem específico, identificado pelo ID 583. O objetivo é realizar uma requisição HTTP para recuperar e exibir os recursos do personagem.
+A implementação faz uso da classe HttpClient para enviar a solicitação e processar a resposta, garantindo que os dados sejam tratados corretamente.
+
+## Clonando o repositório
+
+```bash
+git clone https://github.com/Carlos4415/GameOfThronesAPI.git
